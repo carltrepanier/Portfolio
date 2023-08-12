bb7e8ef443574f10283264a8c77c5bec9a9691a0
At0micTurtle <carltrep@videotron.ca>
2023-08-12 17:49:10 -0400

    WIP 3D Portfolio

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import { Html, useProgress } from '@react-three/drei';
 
 const Loader = () => {
-  return (
-    <div>Loader</div>
-  )
-}
+  const { progress } = useProgress();
 
-export default Loader
+  return (
+    <Html>
+      <span className="canvas-load"></span>
+      <p style={{fontSize: 14, color: '#f1f1f1', fontWeight: 800, marginTop: 40}}>{progress.toFixed(2)}%</p>
+    </Html>
+  )
+};
+
+export default Loader;
