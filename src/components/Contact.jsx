3e63676b9ea8279a3ea56f72abcd36ae7b63b8ad
At0micTurtle <carltrep@videotron.ca>
2023-08-13 10:00:06 -0400

    Contact update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,9 +39,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Carl",
+          to_name: import.meta.env.VITE_APP_EMAILJS_TO_NAME,
           from_email: form.email,
-          to_email: "ctrepanier02@gmail.com",
+          to_email: import.meta.env.VITE_APP_EMAILJS_TO_EMAIL,
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
