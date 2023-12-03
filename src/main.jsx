80b88acf202d85019b5ba12a387248887f0b4902
At0micTurtle <carltrep@videotron.ca>
2023-12-02 22:40:39 -0500

    Updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { inject } from "@vercel/analytics";
+import { createRoot } from "react-dom/client";
 import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "./i18n";
 
 inject();
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = createRoot(document.getElementById("root"));
+root.render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>
