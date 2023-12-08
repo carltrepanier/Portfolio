239bc153f174e801e8dc7d52a2c9b4d5fa28112e
At0micTurtle <carltrep@videotron.ca>
2023-12-08 00:12:54 -0500

    Remove @vercel/speed-insights package

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import { inject } from "@vercel/analytics";
-import { injectSpeedInsights } from "@vercel/speed-insights";
 import { createRoot } from "react-dom/client";
 import React from "react";
 import App from "./App.jsx";
@@ -7,7 +6,6 @@ import "./index.css";
 import "./i18n";
 
 inject();
-injectSpeedInsights();
 
 const root = createRoot(document.getElementById("root"));
 root.render(
