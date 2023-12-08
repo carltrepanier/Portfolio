c058c4236c219f5aa153ed66c642f00feecf2832
At0micTurtle <carltrep@videotron.ca>
2023-12-07 23:54:46 -0500

    Add @vercel/speed-insights package

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 import { createRoot } from "react-dom/client";
 import React from "react";
 import App from "./App.jsx";
@@ -6,6 +7,7 @@ import "./index.css";
 import "./i18n";
 
 inject();
+injectSpeedInsights();
 
 const root = createRoot(document.getElementById("root"));
 root.render(
