309d8514c6be1e31a2e03360d3ee7470763e4079
Carl T <carltrep@videotron.ca>
2023-12-24 00:22:34 -0500

    Update Footer component styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,16 @@
-import { motion } from "framer-motion";
-import { fadeIn } from "../utils/motion";
+import { motion } from 'framer-motion';
+import { fadeIn } from '../utils/motion';
 
 export default function Footer() {
 	return (
-		<footer className="flex">
+		<footer className='flex'>
 			<motion.p
-				variants={fadeIn("", "", 0.2, 1)}
-				initial="hidden"
-				whileInView="show"
-				className="mb-4 text-secondary text-[17px] max-w-3xl leading-[30px] mx-auto text-center"
+				variants={fadeIn('', '', 0.2, 1)}
+				initial='hidden'
+				whileInView='show'
+				className='mb-4 text-secondary text-[17px] max-w-3xl leading-[30px] mx-auto text-center'
 			>
-				&copy; {new Date().getFullYear()} Conception CT
+				&copy; {new Date().getFullYear()} Conception CT - Tous droits réservés
 			</motion.p>
 		</footer>
 	);
